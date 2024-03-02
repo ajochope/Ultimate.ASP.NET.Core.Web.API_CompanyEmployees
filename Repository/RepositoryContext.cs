f30d1f6afa574278e8be7a3dd43b561966e70c32
ocalles <oscar.calles@accuro.es>
2024-03-02 13:08:26 +0100

    Repository class lib

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
