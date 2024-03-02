f609f073a2a9435d88d2475deb25e39350d913c5
ocalles <oscar.calles@accuro.es>
2024-03-02 08:58:14 +0100

    configs to 1.6 chapter

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -2,15 +2,19 @@
 {
     public static class ServiceExtensions
     {
-        //  CORS services to the DI container
+        // CORS services to the DI container
         public static void ConfigureCors(this IServiceCollection services) =>
-         services.AddCors(options =>
-         {
-             options.AddPolicy("CorsPolicy", builder =>
-             builder.AllowAnyOrigin()
-             .AllowAnyMethod()
-             .AllowAnyHeader());
-         });
-
+             services.AddCors(options =>
+             {
+                 options.AddPolicy("CorsPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+             });
+        // IIS integration
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
     }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,12 +1,50 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Use the ConfigureCors extension method
+builder.Services.ConfigureCors();
+
+// IIS integration
+builder.Services.ConfigureIISIntegration();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//handling exceptions and HTTP Strict Transport Security (HSTS)
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+//automatically redirects HTTP requests to HTTPS
+app.UseHttpsRedirection();
+
+// enables using static files for the request.
+app.UseStaticFiles();
+
+// Use the ForwardedHeaders middleware,
+// updates the request's original IP address, scheme, and host based on the  X-Forwarded-For
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+
+// User CORS policy
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
