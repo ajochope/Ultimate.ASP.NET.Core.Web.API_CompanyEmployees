2e0158991ec59b4db1a314262e300b20d76887e6
ocalles <oscar.calles@accuro.es>
2024-03-02 08:33:50 +0100

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //  CORS services to the DI container
+        public static void ConfigureCors(this IServiceCollection services) =>
+         services.AddCors(options =>
+         {
+             options.AddPolicy("CorsPolicy", builder =>
+             builder.AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+         });
+
+    }
+}
