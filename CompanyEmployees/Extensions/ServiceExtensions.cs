d3a47b91b8f06d4d1b88e5506dc4a338d6ce7d4c
ocalles <oscar.calles@accuro.es>
2024-03-02 11:42:07 +0100

    logging messages library NLog

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,20 +1,28 @@
-﻿namespace CompanyEmployees.Extensions
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions
 {
     public static class ServiceExtensions
     {
         // CORS services to the DI container
         public static void ConfigureCors(this IServiceCollection services) =>
-             services.AddCors(options =>
-             {
-                 options.AddPolicy("CorsPolicy", builder =>
-                 builder.AllowAnyOrigin()
-                 .AllowAnyMethod()
-                 .AllowAnyHeader());
-             });
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
         // IIS integration
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
-            services.Configure<IISOptions>(options =>
-            {
-            });
+        services.Configure<IISOptions>(options =>
+        {
+        });
+        // Logger service
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<LoggerService.ILoggerManager, LoggerManager>();
+        }
     }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,14 +1,25 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// configuration for a logger service
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configFileName = $"nlog.{environment}.config";
+var configFilePath = Path.Combine(Directory.GetCurrentDirectory(), configFileName);
+
+LogManager.Setup().LoadConfigurationFromFile(configFilePath);
 
 // Use the ConfigureCors extension method
 builder.Services.ConfigureCors();
 
 // IIS integration
 builder.Services.ConfigureIISIntegration();
+
+// Logger service
+builder.Services.ConfigureLoggerService();
 
 // Add services to the container.
 
@@ -60,39 +71,39 @@ app.UseAuthorization();
 //{
 //    await context.Response.WriteAsync("Hello from the 1nd delegate.");
 //});
-app.Use(async (context, next) =>
-{
-    Console.WriteLine($"Logic before executing the next delegate in the Use method");
-    await next.Invoke();
-    Console.WriteLine($"Logic after executing the next delegate in the Use method");
-});
-app.Map("/usingmapbranch", builder =>
-{
-    builder.Use(async (context, next) =>
-    {
-        Console.WriteLine("Map branch logic in the Use method before the next delegate");
-        await next.Invoke();
-        Console.WriteLine("Map branch logic in the Use method after the next delegate");
-    });
-    builder.Run(async context =>
-    {
-        Console.WriteLine($"Map branch response to the client in the Run method");
-        await context.Response.WriteAsync("Hello from the map branch.");
-    });
-});
-app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
-{
-    builder.Run(async context =>
-    {
-        await context.Response.WriteAsync("Hello from the --MapWhen-- branch.");
-    });
-});
+//app.Use(async (context, next) =>
+//{
+//    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+//    await next.Invoke();
+//    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+//});
+//app.Map("/usingmapbranch", builder =>
+//{
+//    builder.Use(async (context, next) =>
+//    {
+//        Console.WriteLine("Map branch logic in the Use method before the next delegate");
+//        await next.Invoke();
+//        Console.WriteLine("Map branch logic in the Use method after the next delegate");
+//    });
+//    builder.Run(async context =>
+//    {
+//        Console.WriteLine($"Map branch response to the client in the Run method");
+//        await context.Response.WriteAsync("Hello from the map branch.");
+//    });
+//});
+//app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+//{
+//    builder.Run(async context =>
+//    {
+//        await context.Response.WriteAsync("Hello from the --MapWhen-- branch.");
+//    });
+//});
 
-app.Run(async context =>
-{
-    Console.WriteLine($"Writing the response to the client in the Run method");
-    await context.Response.WriteAsync("Hello from the middleware component.");
-});
+//app.Run(async context =>
+//{
+//    Console.WriteLine($"Writing the response to the client in the Run method");
+//    await context.Response.WriteAsync("Hello from the middleware component.");
+//});
 
 app.MapControllers();
 
