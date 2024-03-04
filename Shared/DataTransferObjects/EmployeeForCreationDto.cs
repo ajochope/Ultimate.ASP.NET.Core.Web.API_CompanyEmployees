0cc12064b865a0cb67975ad4b2ec596dcab9f1e0
ocalles <oscar.calles@accuro.es>
2024-03-04 08:33:57 +0100

    13 Validation need check

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -1,4 +1,18 @@
-﻿namespace Shared.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
 {
-    public record EmployeeForCreationDto(string Name, int Age, string Position);
+    public record EmployeeForCreationDto
+    {
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        string? Name { get; init; }
+
+        [Required(ErrorMessage = "Age is a required field.")]
+        int Age { get; init; }
+
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        string? Position { get; init; }
+    }
 }
