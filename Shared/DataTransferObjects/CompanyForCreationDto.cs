a75cda1bebe36cff55716f5b8551d98b4cd81cb3
ocalles <oscar.calles@accuro.es>
2024-03-05 07:25:50 +0100

    13 Create Company validation

--- Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -1,5 +1,22 @@
-﻿namespace Shared.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
 {
-    public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
+    public record CompanyForCreationDto
+    {
+        [Required(ErrorMessage = "Name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Address { get; init; }
+
+        [Required(ErrorMessage = "Country is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string? Country { get; init; }
+    }
+
+    //public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
 
 }
